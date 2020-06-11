9e14a4ea2522f69bc621fcfb4bf2091a2274a8b3
tanchengjin <18865477815@163.com>
2020-06-11 14:05:29 +0800

    fix product page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,40 +8,82 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products=Product::query()->where('on_sale',1)->paginate(16);
-        return view('products.index',compact(['products']));
+        $product = Product::query()->where('on_sale', 1);
+        $data = [
+            'search' => '',
+            'order' => ''
+        ];
+        if ($search = $request->input('q', '')) {
+            $product->where(function ($query) use ($search) {
+                $like = '%' . $search . '%';
+                $query->where('title', 'like', $like)
+                    ->orWhere('description', 'like', $like)
+                    ->orWhereHas('sku', function ($query) use ($like) {
+                        $query->where('title', 'like', $like)
+                            ->orWhere('description', 'like', $like);
+                    });
+            });
+            $data['search'] = $search;
+        }
+
+        if ($order = $request->input('order', '')) {
+            $data['order'] = $order;
+            preg_match('/^(.+)_(asc|desc)$/', $order, $m);
+
+            if (in_array($m[1], ['price', 'sold', 'review'])) {
+                if ($m[1] === 'price') {
+                    $m[1] = 'min_price';
+                }
+
+                if ($m[1] === 'review') {
+                    $m[1] = 'review_count';
+                }
+
+                if ($m[1] === 'sold') {
+                    $m[1] = 'sold_count';
+                }
+
+                $product->orderBy($m[1], $m[2]);
+            }
+
+        }
+        $products = $product->paginate(16);
+
+        return view('products.index', compact(['products', 'data']));
     }
 
-    public function show($id,Request $request)
+    public function show($id, Request $request)
     {
-        $product=Product::query()
-            ->where('on_sale',1)
+        $product = Product::query()
+            ->where('on_sale', 1)
             ->with(['sku'])
             ->find($id);
-        $favorite=false;
-        if($request->user()){
-            $favorite=$request->user()->favorites()->find($product->id);
+        $favorite = false;
+        if ($request->user()) {
+            $favorite = $request->user()->favorites()->find($product->id);
         }
-        $reviews=OrderItem::query()->with(['product','sku','order.user'])
-            ->where('product_id',$id)
+        $reviews = OrderItem::query()->with(['product', 'sku', 'order.user'])
+            ->where('product_id', $id)
             ->whereNotNull('reviewed_at')
-            ->orderBy('reviewed_at','desc')
+            ->orderBy('reviewed_at', 'desc')
             ->get();
-        return view('products.show',compact('product','favorite','reviews'));
+        return view('products.show', compact('product', 'favorite', 'reviews'));
     }
+
     #商品收藏
-    public function favorite(Product $product,Request $request)
+    public function favorite(Product $product, Request $request)
     {
-        if($request->user()->favorites()->find($product->id)){
+        if ($request->user()->favorites()->find($product->id)) {
             return [];
         }
         $request->user()->favorites()->attach($product);
         return [];
     }
+
     #商品取消收藏
-    public function disFavor(Product $product,Request $request)
+    public function disFavor(Product $product, Request $request)
     {
         $request->user()->favorites()->detach($product);
         return [];
@@ -49,7 +91,7 @@ class ProductController extends Controller
 
     public function favorList(Request $request)
     {
-        $favorites=$request->user()->favorites()->get();
-        return view('layouts.user.favorite.index',compact('favorites'));
+        $favorites = $request->user()->favorites()->get();
+        return view('layouts.user.favorite.index', compact('favorites'));
     }
 }
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,6 +4,32 @@
     <div class="container">
         <div class="card">
             <div class="card-body">
+                <form action="{{route('products.index')}}" class="form-search">
+                    <div class="form-row">
+                        <div class="col-md-9">
+                            <div class="form-row">
+                                <div class="col-auto">
+                                    <input type="text" name="q" class="form-control">
+
+                                </div>
+                                <button class="btn btn-primary">搜索</button>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <select name="order" id="" class="form-control order">
+                                <option value="">排序方式</option>
+                                <option value="price_asc">价格升序</option>
+                                <option value="price_desc">价格降序</option>
+                                <option value="sold_asc">销量升序</option>
+                                <option value="sold_desc">销量降序</option>
+                                <option value="review_asc">评价升序</option>
+                                <option value="review_desc">评价降序</option>
+                            </select>
+                        </div>
+                    </div>
+                </form>
+
+
                 <div class="products-content">
                     <div class="row">
                         @foreach($products as $product)
@@ -16,7 +42,9 @@
                                             </a>
                                         </div>
                                         <div class="price">￥{{$product->min_price}}</div>
-                                        <div class="title"><a href="{{route('products.show',$product->id)}}">{{$product->title}}</a></div>
+                                        <div class="title"><a
+                                                href="{{route('products.show',$product->id)}}">{{$product->title}}</a>
+                                        </div>
                                     </div>
                                     <div class="bottom">
                                         <div class="sold_count">销量：<span>{{$product->sold_count}}</span></div>
@@ -33,4 +61,17 @@
             </div>
         </div>
     </div>
+@endsection
+
+@section('javascript')
+    <script>
+        $(document).ready(function () {
+            $('.order').on('change', function () {
+                $('.form-search').submit();
+            });
+
+            $('select[name=order]').val('{{$data['order']}}');
+            $('input[name=q]').val('{{$data['search']}}');
+        });
+    </script>
 @endsection
